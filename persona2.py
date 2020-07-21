a4838aaa8a6cee37662acdb78d825a765a1cc21e
axelrodriguezprd <axel.rodriguezprd@uanl.edu.mx>
2020-07-21 10:48:37 -0500

    Se completa la clase Persona2

--- persona2.py
+++ persona2.py
@@ -3,6 +3,7 @@ class Persona2:
     def __init__(self, nombre, edad, municipio):
         self.__nombre = nombre
         self.__edad = edad
+        self.__municipio = municipio
 
     @property
     def nombre(self):
@@ -17,14 +18,28 @@ class Persona2:
         return self.__edad
 
     @edad.setter
-    def nombre(self, valor):
+    def edad(self, valor):
         self.__edad = valor
 
     @property
-    def minicipio(self):
+    def municipio(self):
         return self.__municipio
 
     @municipio.setter
     def municipio(self, valor):
         self.__municipio = valor
 
+    def imprimirInfo(self):
+        print(f"-{self.__nombre}\n-{self.__edad}\n-{self.__municipio}")
+
+    def obtenerEtapa(self):
+        if self.__edad >= 0 and self.__edad <= 10:
+            print("Es niño")
+        elif self.__edad >= 11 and self.__edad <= 17:
+            print("Es adolescente") 
+        elif self.__edad >= 18 and self.__edad <= 40:
+            print("Es adulto")
+        elif self.__edad > 40:
+            print("Es adulto mayor")
+        else:
+            print("Edad incorrecta")
