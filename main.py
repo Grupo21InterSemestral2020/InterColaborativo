8f76f1db71032d710364011c2cdfe64e4314b2c2
Lesly Abigail Landeros Sanchez <leslylanderossanchez@gmail.com>
2020-07-21 07:07:18 -0500

    nombre

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print("Juan Gabriel Lopez")
-<<<<<<< HEAD
-print("Lesly Abigail Landeros Sanchez")
-=======
 print("Axel David Rodriguez Prado")
 print("Daniela Aracely Rodriguez Renteria")
 print("Angie Sarahi Camarillo Ramirez")
@@ -11,4 +8,4 @@ print("Lesly Abigail Landeros Sanchez")
 print("Daira Yllen Torres Gonzalez")
 print("Claudia Morón Fernández")
 print("Linus Torvalds")
->>>>>>> 3f76c4fd1e97eea2bb8c10e1c3208b47c2e5bc16
+
