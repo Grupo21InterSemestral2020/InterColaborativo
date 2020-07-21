a3d5ad93933f0868a2c330435e3f1e1c308bead8
Lesly Abigail Landeros Sanchez <leslylanderossanchez@gmail.com>
2020-07-21 10:55:36 -0500

    modifique mi clase

--- persona8.py
+++ persona8.py
@@ -1,30 +1,30 @@
 class Persona8:
-    def _int_(self, nombre, edad, municipio):
+    def _init_(self, nombre, edad, municipio):
         self._nombre= nombre
         self._edad= edad
         self._municipio= municipio
 
     @property
     def nombre(self):
-        return self._nombre
+        return self.__nombre
 
     @nombre.setter
     def nombre(self, valor):
-        self._nombre= valor
+        self.__nombre= valor
 
     @property
     def edad(self):
-        return self._edad
+        return self.__edad
 
     @edad.setter
     def edad(self, valor):
-        self._edad= valor
+        self.__edad= valor
 
     @property
     def municipio(self):
-        return self._edad
+        return self.__edad
 
     @edad.setter
     def municipio(self,valor):
-        self._municipio= valor
+        self.__municipio= valor
 
