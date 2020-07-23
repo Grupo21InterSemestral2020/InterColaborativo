7d7d68d50a2feee44e6233b5e14491764eec0cb1
Angie Sarahi Camarillo Ramirez <angieramirez0111@gmail.com>
2020-07-23 10:07:08 -0500

    agregue poperty lado

--- figuras/cuadrado.py
+++ figuras/cuadrado.py
@@ -1,2 +1,13 @@
 class Cuadrado:
-    pass
+    def __init__(self,lado):
+        self.__lado = lado
+    
+    @property
+    def lado(self):
+        return self.__lado
+
+    @lado.setter
+    def lado(self,lado):
+        self.__lado = lado
+
+    
