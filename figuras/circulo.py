823a0e7b485fed4086797afb3f281764ecf8637f
JuanAngel1 <juanito_2600@hotmail.com>
2020-07-23 10:34:47 -0500

    Avance

--- figuras/circulo.py
+++ figuras/circulo.py
@@ -1,2 +1,12 @@
 class Circulo:
-    
+    def __init__(self,pi,radio):
+        self.__pi = pi
+        self.__radio = radio
+
+     @property
+     def pi(self):
+        return self.__pi
+
+     @pi.setter
+     def pi(self,valor):
+        self.__pi = valor
