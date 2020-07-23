e3220a331b1e88c2deed01d4995645ada6981836
Blanca Nallely Macias Vazquez <naychida@gmail.com>
2020-07-23 10:34:38 -0500

    sector

---
+++ figuras/sector.py
@@ -0,0 +1,12 @@
+class Sector
+  def __init__(self,pin,radioCuadrado,numGrados):
+      self.__pin = pin
+      self.__radioCuadrado = radioCuadrado
+      self.__numGrados = numGrados
+
+@property
+def pin(self):
+    return self.__pin
+@pin.setter
+def pin(self, valor):
+    self.__ pin = valor
