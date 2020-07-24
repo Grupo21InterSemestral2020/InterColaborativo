cae8fc96abed3f165ae1fa490d52ee9999ea2b06
Edgar Eduardo Arriaga Martinez <edgar.arriagama@uanl.edu.mx>
2020-07-23 23:00:06 -0500

    area ellipse.py

--- figuras/ellipse.py
+++ figuras/ellipse.py
@@ -1,2 +1,34 @@
 class Ellipse:
-    def __init__(self,)
+    def __init__(self,pi,a,b)
+    self.__pi = pi
+    self.__a = a
+    self.__b = b
+
+@property
+def pi(self):
+    return self.__pi
+@pi.setter
+def pi(self, valor):
+    self.__ pi = valor
+
+@property
+def a(self):
+    return self.a
+@a.setter
+def a(self, valor):
+    self.__ a = valor
+
+@property
+def b(self):
+    return self.__b
+@b.setter
+def b(self, valor):
+    self.__ b = valor
+
+def area(self):
+    area = self.__pi * sel.__b * self.__b
+    return area
+
+def ImprimirInfo(self):
+    a= (f'Datos de ellipse:\nPi:{self.__pi}\na:{self.__a}\nb:{self.__b}\n El area es:{self.area()}')
+    print(a)
