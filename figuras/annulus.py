7700315e9975863c9cbc7e5d6fc949f889c7f281
Andres Jared Vela Canizales <andresvela_202@hotmail.com>
2020-07-27 22:20:59 -0500

    add annulus

---
+++ figuras/annulus.py
@@ -0,0 +1,26 @@
+#Annulus Area = πR² - πr² = π(R² - r²)
+
+class Annulus:
+    def __init__(self, π, R, r):
+        self.__π = π
+        self.__R = R
+        self.__r = r
+
+    @property
+    def π(self):
+        return self.__π
+
+    @π.setter
+    def π(self, valor):
+        self.__π = valor
+
+    @property
+    def R(self):
+        return self.__R
+
+    @R.setter
+    def R(self,valor2):
+        self.__R valor2
+
+    
+
